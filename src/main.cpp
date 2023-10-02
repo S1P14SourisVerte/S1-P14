5b0f7463ca6235b52fd8fc9e64e29bf85cc531c8
VL <vincentlavalleewrk@outlook.com>
2023-10-02 18:08:41 -0400

    Siflet fichier test
    Programme test pour le siflet, les deux siflets sont fonctionnel! Reste juste  à créer une fonction... à discuter...

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 
 void setup() {
+    Serial.begin(9600);
 
+    pinMode(OUTPUT,A0);
 }
 
 void loop() {
 
+    int valueOut = analogRead(A0);
+
+    if (valueOut >= 500){
+        Serial.print("GO!!!");
+    }
+    
+    delay(100);
 }
