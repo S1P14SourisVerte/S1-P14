9282c610a2fcfba5f700f0866395262353ae9a89
VL <vincentlavalleewrk@outlook.com>
2023-10-05 09:02:33 -0400

    Code test sifflet
    Premier code test pour le sifflet

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,19 @@
 void setup() {
     Serial.begin(9600);
 
-    pinMode(OUTPUT,A0);
+    pinMode(INPUT,A0);
 }
 
 void loop() {
 
-    int valueOut = analogRead(A0);
+    int valueIn = analogRead(A0);
 
-    if (valueOut >= 500){
+    //Serial.println(valueIn);
+    
+    if (valueIn >= 500){
         Serial.print("GO!!!");
     }
+    
     
     delay(100);
 }
