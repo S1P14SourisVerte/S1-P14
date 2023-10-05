5ad19c9c5e607c6acefb31b3342a72bd801fe319
VinceP31 <vincepou1@hotmail.com>
2023-10-05 14:29:59 -0400

    Update main.cpp
    mise à jour du code pour la detection du sifflet

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,36 @@
 #include <Arduino.h>
 
+#define PIN_WHISTLE A0
+int detect_whistle();
 void setup() {
     Serial.begin(9600);
 
-    pinMode(INPUT,A0);
+    pinMode(INPUT,PIN_WHISTLE);
 }
 
 void loop() {
 
-    int valueIn = analogRead(A0);
+    
 
     //Serial.println(valueIn);
+    Serial.print(String(detect_whistle()));
     
-    if (valueIn >= 500){
-        Serial.print("GO!!!");
-    }
     
     
     delay(100);
+}
+
+int detect_whistle()
+{
+    int valueIn = analogRead(PIN_WHISTLE);
+    //Serial.println(String(valueIn));
+    if (valueIn >= 500)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+
 }
