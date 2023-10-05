0eac57390982311ea9a85fc4e01d26b9cc84e195
VinceP31 <vincepou1@hotmail.com>
2023-10-05 13:30:58 -0400

    detection_mur
    Voici la fonction pour le détecteur de proximité (testé et fonctionnelle)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
 #include <Arduino.h>
 
-void setup() {
+#define GREEN 28
+#define RED 26
 
+int detection_mur();
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(GREEN, INPUT);
+  pinMode(RED, INPUT);
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(String(detection_mur()));
+  delay(500);
 
+}
+
+int detection_mur()
+{
+  int green = digitalRead(GREEN);
+  int red = digitalRead(RED);
+
+  if(green == 0 && red == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
 }
